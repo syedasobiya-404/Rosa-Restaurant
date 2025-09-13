f96842e9734c15fe0f765137a9fe5b08b6115037
syedasobiya-404 <syedasobiya404@gmail.com>
2025-09-13 18:46:52 +0500

    Write a clear message about what changed

---
+++ main.js
@@ -0,0 +1,3 @@
+const mobileBtn = document.getElementById("mobileBtn");
+const mobileNav = document.getElementById("mobileNav");
+mobileBtn.addEventListener("click", () => mobileNav.classList.toggle("hidden"));
